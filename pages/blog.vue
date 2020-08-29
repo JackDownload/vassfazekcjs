1a5ea44e09bb3158b3ecdc144818aca5a9930aee
JackDownload <kismocsy@gmail.com>
2020-08-29 15:00:27 +0200

    xx

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,10 +1,16 @@
 <template>
   <v-layout>
     <v-flex text-xs-left>
-      <div v-for="blog in blogs" :key="blog.slug">
-        <h3>{{ blog.title }}</h3>
-        <img alt="`${image}`"/>
+       <v-btn color="primary" flat nuxt to="/">Back</v-btn>
+      <h1>{{title}}</h1><br>
+      <div>
+      <img
+        :src="`${image}`"
+        alt="Fitness_quotes"
+        class="mb-5"
+      >
       </div>
+      <div v-html="body"></div>
     </v-flex>
   </v-layout>
 </template>
@@ -19,8 +25,8 @@ export default {
             }
         })
       .then(res => {
-        return {blog : res.data.objects}
-      });W
+        return {title : res.data.objects[0].title, body: res.data.objects[0].content, image:res.data.objects[0].metadata.image.url}
+      });
   }
 };
         
