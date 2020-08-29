1195d6354da2abbed89a8d0e568ee753639562f7
JackDownload <kismocsy@gmail.com>
2020-08-29 16:49:44 +0200

    Update blog.vue

--- pages/blog.vue
+++ pages/blog.vue
@@ -2,9 +2,18 @@
   <v-layout>
     <v-flex text-xs-left>
        <v-btn color="primary" flat nuxt to="/">Back</v-btn>
-            <li v-for="blog of blogs" :key="blog.id">
-        <n-link :to="`/blog/${blog.slug}`">{{ blog.title }} </n-link>
+            <li v-for="blog of blogs" :key="blog.slug">
+        <n-link :to="`/blogs/${blog.slug}`">{{ blog.title }}</n-link>
       </li>
+      <div>
+      <img
+        :src="`${blogs.image}`"
+        alt="Fitness_quotes"
+        class="mb-5"
+      >
+      </div>
+      <h1>{{blogs.body}}</h1>
+      <br>
     </v-flex>
   </v-layout>
 </template>
@@ -12,7 +21,10 @@
 import axios from "axios";
 import config from '../config/config';
 export default {
-  asyncData (context) {
+    data ()
+    {bolg: []
+    },
+  asyncData () {
     return axios.get(config.url + config.bucket_slug + '/object-type/blogs',{
             params: {
                 read_key: config.read_key
