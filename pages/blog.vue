8f9d5b66a6a04ecce22c4decb76fbf7b71529ef6
JackDownload <kismocsy@gmail.com>
2020-08-29 14:05:25 +0200

    update

--- pages/blog.vue
+++ pages/blog.vue
@@ -2,30 +2,36 @@
   <v-layout>
     <v-flex text-xs-left>
        <v-btn color="primary" flat nuxt to="/">Back</v-btn>
-      <h1>{{title}}</h1><br>
+            <li v-for="blog of blogs" :key="blog.slug">
+        <n-link :to="`/blogs/${blog.slug}`">{{ blog.title }}</n-link>
+      </li>
       <div>
       <img
-        :src="`${image}`"
+        :src="`${blogs.image}`"
         alt="Fitness_quotes"
         class="mb-5"
       >
       </div>
-      <div v-html="body"></div>
+      <h1>{{blogs.body}}</h1>
+      <br>
     </v-flex>
   </v-layout>
 </template>
 <script>
 import axios from "axios";
-import config from '../config/config';
+import config from '../../config/config';
 export default {
-  asyncData (context) {
+    data ()
+    {bolgs: []
+    },
+  asyncData () {
     return axios.get(config.url + config.bucket_slug + '/object-type/blogs',{
             params: {
                 read_key: config.read_key
             }
         })
       .then(res => {
-        return {title : res.data.objects[0].title, body: res.data.objects[0].content, image:res.data.objects[0].metadata.image.url}
+        return {blogs : res.data.objects}
       });
   }
 };
