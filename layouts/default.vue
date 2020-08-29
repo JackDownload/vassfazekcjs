850459ac4c292ecf845b77e88bbd77604d306de0
JackDownload <kismocsy@gmail.com>
2020-08-29 19:43:17 +0200

    dfgf

--- layouts/default.vue
+++ layouts/default.vue
@@ -79,7 +79,7 @@ export default {
         {
           icon: 'bubble_chart',
           title: 'Blogs',
-          to: '/blogs/:slug'
+          to: '/blogs/_slug'
         }
       ],
       miniVariant: false,
