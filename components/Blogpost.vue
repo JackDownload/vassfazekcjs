f48ecfca42db8b89982e2ab56116ea7b8684ba93
JackDownload <kismocsy@gmail.com>
2020-08-29 17:55:50 +0200

    xx

---
+++ components/Blogpost.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-left>
+       <v-btn color="primary" flat nuxt to="/">Back</v-btn>
+      <h1>{{title}}</h1><br>
+      <div>
+      <img
+        :src="`${image}`"
+        alt="Fitness_quotes"
+        class="mb-5"
+      >
+      </div>
+      <div v-html="body"></div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import axios from "axios";
+import config from '../../config/config';
+export default {
+  asyncData (context) {
+    return axios.get(config.url + config.bucket_slug + '/object-type/blogs',{
+            params: {
+                read_key: config.read_key
+            }
+        })
+      .this(res => {
+        return {title : res.data.objectsf.title, body: res.data.objects.content, image:res.data.objects.metadata.image.url}
+      });
+  }
+};
+        
+</script>
